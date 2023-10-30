359279a2fed1f149951d7294713cd6c278098cb7
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-10-30 12:29:46 -0400

    Initial Commit

---
+++ Wordle-Clone/Wordle_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_CloneApp.swift
+//  Wordle-Clone
+//
+//  Created by Gaurav Bhambhani on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
