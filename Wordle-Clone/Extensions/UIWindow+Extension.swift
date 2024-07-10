68c935a2be0f1f0c24906758a8b13e3de27187cf
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-07-10 07:05:39 -0400

    Implemeted Settings page to toggle light and dark mode. Implemented Toast

---
+++ Wordle-Clone/Extensions/UIWindow+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIWindow+Extension.swift
+//  Wordle-Clone
+//
+//  Created by Gaurav Bhambhani on 7/9/24.
+//
+
+import Foundation
---
+++ Wordle-Clone/ViewModels/ColorSchemeManager.swift
@@ -0,0 +1,8 @@
+//
+//  ColorSchemeManager.swift
+//  Wordle-Clone
+//
+//  Created by Gaurav Bhambhani on 7/10/24.
+//
+
+import Foundation
